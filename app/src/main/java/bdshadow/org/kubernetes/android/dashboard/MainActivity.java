85d81d82618fc672ecf4789a5bd47e97413bb817
Dmitrii Bocharov <bdshadow@gmail.com>
2018-09-05 12:49:52 +0200

    Initial commit

---
+++ app/src/main/java/bdshadow/org/kubernetes/android/dashboard/MainActivity.java
@@ -0,0 +1,24 @@
+package bdshadow.org.kubernetes.android.dashboard;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void addKubernetesConnectionClick(View v) {
+        Intent newKubernetesConncetionIntent = new Intent(this, NewKubernetesConnectionActivity.class);
+        startActivity(newKubernetesConncetionIntent);
+    }
+}
